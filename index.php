0e1aa75056ecb3eace29ddfeacd3e5b27ee93142
adittiadwikurniawan <adittiadwikurniawan>
2026-03-28 19:19:29 +0700

    menyesuaikan index

--- index.php
+++ index.php
@@ -1,7 +1,17 @@
 <?php
+// 1. menlankan logikanya terlebih dahulu
+require_once 'inputuser.php';
 
-require_once 'Proses/InputUser.php';
-
-$aplikasi = new InputUser();
-$aplikasi->jalankan();
+// 2. menampilkan html-nya (logika di atas akan mengirim data ke file tampilan ini)
 ?>
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Project Form PHP</title>
+</head>
+<body>
+    <?php require_once 'formtapilan.php'; ?>
+</body>
+</html>
